f0b010999daf2fe47865818af63e54d2162dea8b
Felipe Gomes Bulhões <152231702+felipegbulhoes@users.noreply.github.com>
2024-01-17 18:43:30 -0300

    Add files via upload

--- FreeCodeCamp/JavaScript Algorithms and Data Structures/JSA and DS Project - 2/script.js
+++ FreeCodeCamp/JavaScript Algorithms and Data Structures/JSA and DS Project - 2/script.js
@@ -6,24 +6,29 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
-function cleanInputString(str) {
-const regex = /[+-\s]/g;
-return str.replace(regex, '');
+    function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
 }
 
-function isInvalidInput(str) {
-const regex = /\d+e\d+/i;
-return str.match(regex);
+    function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
 }
 
 function addEntry() {
-const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-const HTMLString = `
-<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-<input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+    />`;
+    targetInputContainer.innerHTML += HTMLString;
 }
 
-function name(params) {
-    
-}
