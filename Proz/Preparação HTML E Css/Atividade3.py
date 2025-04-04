0fb26828fa3f5a2025118196186ab8f1aaf588f7
Felipe Gomes Bulhões <152231702+felipegbulhoes@users.noreply.github.com>
2025-04-04 09:04:19 -0300

    Delete Proz/Preparação HTML E Css directory

--- Proz/Preparação HTML E Css/Atividade3.py
+++
@@ -1,3 +0,0 @@
-lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
-for produto in lista_produtos:
-    print("Temos ", produto, "à venda")
--- Proz/Preparação HTML E Css/Atividade4.py
+++
@@ -1,6 +0,0 @@
-lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
-lista_produtos[lista_produtos.index('batons')] = 'rímel'
-lista_produtos[lista_produtos.index('loções')] = 'cremes hidratantes'
-lista_produtos.remove('delineadores')
-lista_produtos.extend(['hidratação', 'condicionadores'])
-print(lista_produtos)
